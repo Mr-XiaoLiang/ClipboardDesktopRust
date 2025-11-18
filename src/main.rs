83d975ff87a0fc4fa84253aefd579832e860e896
lollipop <liujian594@hotmail.com>
2025-11-18 23:25:32 +0800

    feat: 无边框的窗口体验一般，打算先写内容吧。

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
+use anyhow::Result;
+use slint::LogicalPosition;
+
 slint::include_modules!();
 
-fn main() {
-    // 运行slintUI窗体
-    MainWindow::new().unwrap().run().unwrap();
+fn main() -> Result<()> {
+    let main = Main::new()?;
+
+    let handel = main.as_weak();
+    main.on_close_window(move ||{
+        handel.upgrade().unwrap().hide().unwrap();
+    });
+    //最小化
+    let handel = main.as_weak();
+    main.on_minimized_window(move |enable|{
+        handel.upgrade().unwrap().window().set_minimized(enable);
+    });
+    //最大化
+    let handel = main.as_weak();
+    main.on_maximized_window(move |enable|{
+        handel.upgrade().unwrap().window().set_maximized(enable);
+    });
+
+    let handel = main.as_weak();
+    main.on_move_window(move |offset_x, offset_y|{
+        let main = handel.upgrade().unwrap();
+        let logical_pos = main.window().position().to_logical(main.window().scale_factor());
+        main.window().set_position(LogicalPosition::new(logical_pos.x + offset_x, logical_pos.y + offset_y));
+    });
+
+    main.run()?;
+    Ok(())
 }
